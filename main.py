2a274cab28cf06ef4d2cd05d73d6c48af5bc1adc
69Hakdog69 <johnmichaellim041202@gmail.com>
2023-10-10 23:06:31 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv("C:\\Users\mrhum\Documents\CS-ELEC 1\heart_failure_clinical_records_dataset.csv")
+
+data.isnull().sum()
+
+data.describe()
 
